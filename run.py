af1958143ad32ca0632434878d6389c591181e26
fatoubinetou12 <100080000+fatoubinetou12@users.noreply.github.com>
2025-10-14 23:45:26 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
+# run.py
 from app import create_app, db
-from sqlalchemy import inspect
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()
-    print("Tables créées :", inspect(db.engine).get_table_names())
+# ⚠️ Création de la base SEULEMENT en local
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(host="0.0.0.0", port=8000)
 
-if __name__ == '__main__':
-    app.run(debug=True)
