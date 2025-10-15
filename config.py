9b6a9604fb3e599611fa66ccdb1d83c87b755691
fatoubinetou12 <100080000+fatoubinetou12@users.noreply.github.com>
2025-10-15 02:29:02 +0200

    Update config.py

--- config.py
+++ config.py
@@ -17,14 +17,14 @@ class Config:
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Config mail (désactive les envois réels sur Render)
-    MAIL_SERVER = os.getenv('MAIL_SERVER', 'smtp.gmail.com')
-    MAIL_PORT = int(os.getenv('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', '1') == '1'
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME', '')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD', '')
-    MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER', MAIL_USERNAME)
-    MAIL_SUPPRESS_SEND = os.getenv('MAIL_SUPPRESS_SEND', '1') == '1'
+     # E-mail
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.gmail.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER', MAIL_USERNAME)
+    ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
 
     # Google Maps API
     GOOGLE_MAPS_KEY = os.getenv('GOOGLE_MAPS_KEY', '')
